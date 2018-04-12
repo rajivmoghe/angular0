f45030dcf3b2bc0fd9bff69ee2abf0724c327b7c
Rajiv Moghe <rajivmoghe@yahoo.com>
2018-04-12 23:31:42 +0530

    template forms part 1 lecture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,7 +22,8 @@ import { DishService } from './services/dish.service';
 import { PromotionService } from './services/promotion.service';
 import { LeaderService } from './services/leader.service';
 
-import { AppRoutingModule } from './app-routing/app-routing.module'
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { LoginComponent } from './login/login.component'
 
 @NgModule({
   declarations: [
@@ -33,7 +34,8 @@ import { AppRoutingModule } from './app-routing/app-routing.module'
     FooterComponent,
     HomeComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
     CompatibilityModule,
@@ -46,6 +48,9 @@ import { AppRoutingModule } from './app-routing/app-routing.module'
     AppRoutingModule
   ],
   providers: [DishService, PromotionService, LeaderService],
+  entryComponents: [
+    LoginComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
